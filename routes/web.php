554f37ef66d18b9776851fe1a0443b2598b9aecc
khulood123 <khuloodalsaadi369@gmail.com>
2024-08-26 16:24:49 +0400

    update route for welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Dashboard');
+    return view('welcome');
 });
 
 Route::get('/Dashboard', function () {
