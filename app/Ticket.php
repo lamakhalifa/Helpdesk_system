ca29d5000144c0b5ad6383eae010423180c9e671
fatimah <fatimahbasheikh@gmail.com>
2024-09-04 15:05:14 +0300

    Add comment relation in ticket model

--- app/Ticket.php
+++ app/Ticket.php
@@ -32,4 +32,8 @@ class Ticket extends Model
         return $this->belongsTo(Category::class, 'category_id');
     }
 
+    public function comments(){
+        return $this->hasMany(Comment::class, 'ticket_id');
+    }
+
 }
