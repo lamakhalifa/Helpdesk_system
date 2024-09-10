8dfbca37fcd18ad740a0ffc9c29ee15cd7170d93
lama <l4m4100@gmail.com>
2024-09-10 14:16:56 +0300

    Update Ticket Model

--- app/Ticket.php
+++ app/Ticket.php
@@ -4,10 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Ticket extends Model implements HasMedia
 {
 
+    use InteractsWithMedia;
     use SoftDeletes;
     protected $fillable = [
         'title',
