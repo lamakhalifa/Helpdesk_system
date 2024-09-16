30ccb5347265a12047d2fd0fb9e95bf28f7a47ff
fatimah <fatimahbasheikh@gmail.com>
2024-09-16 15:38:24 +0300

    update views for categories and comments

--- resources/views/comments/create.blade.php
+++ resources/views/comments/create.blade.php
@@ -1,10 +1,10 @@
 
 <div class="card-body">
-    <form action="{{route('tickets.comments.store', $ticket->id)}}" method="post">
+    <form action="{{route('tickets.storeComment', $ticket->id)}}" method="post">
         @csrf
         <div class="form-group">
             <label for="content"></label>
-            <textarea class="form-control" placeholder="{{__('Type your comment here...')}}" name="content" id="content" cols="30" rows="10"></textarea>
+            <textarea class="form-control" placeholder="{{__('Type your comment here...')}}" name="content" id="content" cols="10" rows="5"></textarea>
         </div>
         <div class="form-group">
             <button class="btn btn-success mt-4" type="submit">{{__('Save')}}</button>
--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,18 +1,18 @@
 <div id="commentForm">
-    <div class="card">
+    <div class="card pt-0" >
         <h5 class="card-header text-center text-secondary">{{__('Type your comment here')}}</h5>
         <div id="comments" class="my-4 mx-4">
             @forelse($ticket->comments as $comment)
-                <div class="card p-3 mb-4">
+                <div class="card p-3 mb-4 " style="height:unset !important;">
                     {{$comment->content}}
                     <small>{{__('Author :')}} {{ $comment->user->name }}</small>
                     <small>Posted on {{ $comment->created_at->format('Y-m-d H:i') }}</small>
                     <!-- Check if this is the last comment -->
                     @if ($loop->last)
                         @can('delete', $comment)
-                            <form action="{{ route('tickets.comments.destroy', [$ticket->id, $comment->id]) }}" method="POST" style="display:inline;">
-                                @csrf
+                            <form action="{{ route('comments.destroy', $comment->id)}}" method="POST" style="display:inline;">
                                 @method('DELETE')
+                                @csrf
                                 <button type="submit" class="btn btn-danger" onclick="return confirm('{{__('Are you sure you want to delete this comment?')}}')">Delete</button>
                             </form>
                         @endcan
