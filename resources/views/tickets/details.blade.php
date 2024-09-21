dffd2442cdf26ce15a4d5f8aeadaef07c77a08b2
khulood123 <khuloodalsaadi369@gmail.com>
2024-09-21 17:13:41 +0400

    return back tickets' views

--- resources/views/tickets/details.blade.php
+++ resources/views/tickets/details.blade.php
@@ -6,21 +6,21 @@
         <div class="row justify-content-center">
             <div class="col mb-4">
                 <div class="card pt-0">
-                    <div class="card-header bg-success text-center">Ticket</div>
+                    <h5 class="card-header bg-light text-center text-secondary">Ticket</h5>
                     <div class="card-body text-left">
                         <div class="row">
                             <div class="col-md-12 col-sm-6">
                                 <p>
-                                <h3> <strong style="color: seagreen ; font-size: 22px "> Ticket Title : </strong>{{ $ticket->title }}</h3>
+                                <h6> <strong> Ticket Title : </strong>{{ $ticket->title }}</h6>
                                 </p>
                                 <p>
-                                <h3><strong style="color: seagreen ; font-size: 22px  ">Customer Name : </strong> {{ $ticket->customer->name }}</h3>
+                                <h6><strong>Customer Name : </strong> {{ $ticket->customer->name }}</h6>
                                 </p>
                                 <p>
-                                <h3><strong style="color: seagreen ; font-size: 22px  "> Assignee Agent Name : </strong>{{ $ticket->agent->name }}</h3>
+                                <h6><strong> Assignee Agent Name : </strong>{{ $ticket->agent->name }}</h6>
                                 </p>
-                                <p><h3> <strong style="color: seagreen ; font-size: 22px  ">Ticket Category : </strong>{{ $ticket->category->title}}</h3></p>
-                                <p><h3> <strong style="color: seagreen ; font-size: 22px  ">Ticket Content : </strong>{{ $ticket->content}}</h3></p>
+                                <p><h6> <strong>Ticket Category : </strong>{{ $ticket->category->title}}</h6></p>
+                                <p><h6> <strong >Ticket Content : </strong>{{ $ticket->content}}</h6></p>
                             </div>
                         </div>
                     </div>
@@ -29,19 +29,6 @@
         </div>
         @include('comments.show')
     </div>
-
-
-
-
-    <style>
-
-        .card-header {
-
-            color: #fff;
-            font-size: 22px;
-        }
-
-    </style>
 
 
 @endsection
