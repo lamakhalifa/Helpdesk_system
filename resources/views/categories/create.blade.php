29c7592b265d448cf1b57b986543c896cc24fd59
fatimah <fatimahbasheikh@gmail.com>
2024-09-25 15:54:55 +0300

    update on views

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -1,16 +1,10 @@
 @extends('layouts.app')
 @section('title',__('Create New Category'))
     @section('content')
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card pt-0">
-                        <div class="card-header text-center text-light">{{__('Create New Category')}}</div>
-                        <form action="{{route('categories.store')}}" method="post">
-                            @include('categories._form')
-                        </form>
-                    </div>
-                </div>
-            </div>
+        <div class="container bg-form" id="no-sidebar">
+            <h2>{{__('Create New Category')}}</h2>
+            <form action="{{route('categories.store')}}" method="post">
+                @include('categories._form')
+            </form>
         </div>
 @endsection
